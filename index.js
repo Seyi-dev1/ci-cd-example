d74f212b34060e2c51668bb5c98f85f69628e547
Seyi-dev1 <samoluwaseyi25@gmail.com>
2025-09-25 10:47:04 +0100

    changing thingsss

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! This is sooo cooool.");
 });
 
 app.listen(3000, () => {
